229f8be3847fdb45790714c6b921e6086e257a4f
devnazir <nazirbachrululum@gmail.com>
2020-05-07 17:35:05 +0700

    tes

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function showMovie(){
+    $.ajax({
+        url: 'http://www.omdbapi.com/?apikey=98f26bef&s=' + $('.keyword').val(),
+        success: result => {
+            if (result.Response == 'True') {
+                const movies = result.Search;
+                let data = '';
+                movies.forEach(movie => {
+                    data += `
+                            <div class="col-sm-4 my-3">
+                                <div class="card">
+                                    <img src="${movie.Poster}" class="card-img-top">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${movie.Title}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
+                                    <a href="#" class="btn btn-primary">Detail</a>
+                                </div>
+                            </div>
+                            </div>`
+                });
+
+                $('.movie-container').html(data);
+
+            } else {
+                $('.movie-container').html(`<h1 class="text-center">` + result.Error + `</h1>`);
+            }
+        },
+
+        error: (e) => {
+            console.log(e.responseText);
+        },
+
+    });
+}
+
+
+$('.searchMovie').on('click', function () {
+    showMovie();
+});
+
+$('.keyword').on('keyup', function (e) {
+    if(e.keyCode === 13){
+        showMovie();
+    }
+});
