a043dc99c23aca5477890c9ad7cca6ff8acab538
devnazir <39704763+devnazir@users.noreply.github.com>
2020-05-07 23:12:45 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function showMovie(){
     $.ajax({
-        url: 'https://www.omdbapi.com/?apikey=98f26bef&s=' + $('.keyword').val(),
+        url: 'http://www.omdbapi.com/?apikey=98f26bef&s=' + $('.keyword').val(),
         success: result => {
             if (result.Response == 'True') {
                 const movies = result.Search;
